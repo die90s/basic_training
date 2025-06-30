5f4519584e02463a9267a85d20c0c3487264a1cf
Diego Gonzalez <die90sss@gmail.com>
2025-06-30 01:11:01 -0700

    added median function to median_branch

--- file.py
+++ file.py
@@ -1,1 +1,7 @@
-print("hello world")
+import math
+
+def median(input):
+  if (len(input) % 2):
+    return input[math.floor(len(input) / 2)]
+  else:
+    return (input[math.floor(len(input) / 2)] + input[math.floor((len(input) / 2) + 1)]) / 2
